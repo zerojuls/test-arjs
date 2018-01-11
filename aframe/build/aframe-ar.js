7fb7e65a6b09c4be1f65996c59b0bb9474eecc59
Cesar Mamani Dominguez <zerojuls@gmail.com>
2018-01-11 15:38:43 -0500

    Add new conditional emit for aframe-ar and implementation
    Add new conditional emit for aframe-ar and implementation

--- aframe/build/aframe-ar.js
+++ aframe/build/aframe-ar.js
@@ -8256,6 +8256,7 @@ AFRAME.registerComponent('arjs-anchor', {
 		//////////////////////////////////////////////////////////////////////////////
 		if( _this._arAnchor.parameters.changeMatrixMode === 'modelViewMatrix' ){
 			_this.el.object3D.visible = this._arAnchor.object3d.visible
+            if(_this.el.object3D.visible) { this.el.emit('markerFound'); } //TODO: Detection event recognize marker added
 		}else if( _this._arAnchor.parameters.changeMatrixMode === 'cameraTransformMatrix' ){
 			_this.el.sceneEl.object3D.visible = this._arAnchor.object3d.visible
 		}else console.assert(false)
